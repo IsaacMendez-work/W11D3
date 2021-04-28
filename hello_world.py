d615c565fb2cc41e4aaf6e8354be1ed55eb27f1d
Eyke Mendez <isaacmendez.work+gitbash@gmail.com>
2021-04-28 16:35:05 -0400

    adding pseudocode

--- hello_world.py
+++ hello_world.py
@@ -1,3 +1,4 @@
 print("Hello, World!")
 
-# In VS Code, right click on this file in the explorer window, then click Run Python File In Terminal to execute this app. 
+# In VS Code, right click on this file in the explorer window, then click Run Python File In Terminal to execute this app.
+# In Geany, press F5 to run execute this app or Fn+F5 if the function lock is on. 
